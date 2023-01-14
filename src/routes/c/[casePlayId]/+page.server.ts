2da7ef473cb010deaea2db44ddd4736498dc3e0b
Jake <66141166+harvanchik@users.noreply.github.com>
2023-01-14 11:39:08 -0800

    remove console log

--- src/routes/c/[casePlayId]/+page.server.ts
+++ src/routes/c/[casePlayId]/+page.server.ts
@@ -4,7 +4,6 @@ import type { PageServerLoad } from './$types';
 export const load: PageServerLoad = async ({ params }) => {
 	// get case play from case plays collection
 	const data = await casePlaysCol.findOne({ id: params.casePlayId });
-	console.log(data);
 	// return case play as json
 	return { casePlay: JSON.parse(JSON.stringify(data)) };
 };
