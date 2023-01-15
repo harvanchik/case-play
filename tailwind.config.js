1fb6d937e7c0dc4d72a22e96c1b42c2d8e72d596
harvanchik <66141166+harvanchik@users.noreply.github.com>
2023-01-15 13:24:52 -0800

    prepare svelte overwrite

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  content: ['./**/*.html', './assets/js/*.js'],
-  darkMode: 'class', // 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  plugins: [require('@tailwindcss/forms')],
-};
