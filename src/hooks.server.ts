9c09a907d329836d7f8a8ada85702bce246575df
harvanchik <66141166+harvanchik@users.noreply.github.com>
2023-01-15 19:09:21 -0800

    minify html on build

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,4 +1,5 @@
 import { start_mongo } from '$db/mongo';
+import { minify } from 'html-minifier-terser';
 
 start_mongo()
 	.then(() => {
@@ -7,3 +8,22 @@ start_mongo()
 	.catch(error => {
 		console.error(error);
 	});
+
+const minification_options = {
+	collapseWhitespace: true,
+	collapseInlineTagWhitespace: true,
+	removeComments: true,
+	ignoreCustomComments: [/^#/],
+	minifyCSS: true,
+	minifyJS: true,
+	sortAttributes: true,
+	sortClassName: true
+};
+
+export async function handle({ event, resolve }: any) {
+	const response = await resolve(event, {
+		transformPageChunk: ({ html }: any) => minify(html, minification_options)
+	});
+
+	return response;
+}
