63071bbfb0ab48ad7a9c20bc25e7de6e747a41ec
harvanchik <66141166+harvanchik@users.noreply.github.com>
2023-08-15 20:51:31 -0700

    search input cycles through random search phrases

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -37,6 +37,30 @@
 		}
 	};
 
+	import { onMount } from 'svelte';
+
+	const phrases = [
+		'flag guarding',
+		'roughing the passer',
+		'touchdown',
+		'fumble',
+		'interception',
+		'pass interference',
+		'holding',
+		'illegal forward pass',
+		'illegal motion',
+		'backward pass',
+		'illegal shift',
+		'illegal batting'
+	];
+	let randomSearchPhrase = phrases[Math.floor(Math.random() * phrases.length)];
+
+	onMount(async () => {
+		setInterval(async () => {
+			randomSearchPhrase = phrases[Math.floor(Math.random() * phrases.length)];
+		}, 2000);
+	});
+
 	const casePlays = data.records;
 </script>
 
@@ -64,7 +88,8 @@
 						<input
 							name="query"
 							type="search"
-							class="mx-auto h-12 w-full rounded-md border border-stone-400 bg-stone-200 px-3 text-lg shadow-lg transition-colors duration-300 ease-in-out hover:bg-stone-300/60 hover:shadow-xl focus:border-stone-600 focus:bg-stone-300/80 focus:outline-none focus:ring-0 focus:drop-shadow-2xl"
+							class="mx-auto h-12 w-full rounded-md border border-stone-400 bg-stone-200 px-3 text-lg shadow-lg transition-colors duration-300 ease-in-out placeholder:text-black/60 hover:bg-stone-300/60 hover:shadow-xl focus:border-stone-600 focus:bg-stone-300/80 focus:outline-none focus:ring-0 focus:drop-shadow-2xl"
+							placeholder={randomSearchPhrase}
 							disabled={isQuerying}
 							bind:this={searchRef}
 						/>
@@ -96,7 +121,7 @@
 				>
 					{#each form.casePlays as casePlay}
 						<div
-							class="group mr-10 flex cursor-pointer flex-col space-y-2 border border-stone-300 px-4 py-2 transition-all duration-300 hover:border-stone-400 hover:backdrop-blur-sm"
+							class="group mr-10 flex cursor-pointer flex-col space-y-2 border border-stone-300 px-4 py-2 transition-colors duration-300 hover:border-stone-400 hover:backdrop-blur-sm"
 						>
 							<div class="flex flex-row items-baseline justify-between">
 								<a href="#" class="line-clamp-1 text-3xl font-bold text-stone-800 transition-colors duration-300 group-hover:text-black"
