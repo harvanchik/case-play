b71fe591f3ee443e456e6a5221defb44fc66717e
harvanchik <66141166+harvanchik@users.noreply.github.com>
2023-08-31 00:11:18 -0700

    rough draft case play page

---
+++ src/routes/c/[casePlayId]/+page.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+	import Mark from 'mark.js';
+
+	export let data: PageData;
+	const instance = new Mark('p.context');
+
+	instance.mark('Fourth');
+</script>
+
+<svelte:head>
+	<title>Case Plays</title>
+	<meta name="description" content="Sports case play database for referee education" />
+	<meta
+		name="keywords"
+		content="case,play,plays,db,database,sports,intramural,extramural,official,referee,education,learning,training,flag,tackle,football,basketball,baseball,soccer,volleyball,ice,roller,hockey,flash,cards,coach,player,umpire,ref,ump,zebra,stripes,whistle,rule,rules,interpretation,rule book,edition,spoiler,tool"
+	/>
+	<meta name="author" content="Jake Harvanchik" />
+	<script src="https://code.iconify.design/iconify-icon/1.0.3/iconify-icon.min.js"></script>
+	<script src="/node_modules/mark.js/dist/mark.min.js" charset="UTF-8"></script>
+</svelte:head>
+
+<main class="min-h-screen overflow-hidden bg-stone-100/[97%] scrollbar scrollbar-track-stone-800 scrollbar-thumb-black">
+	<!-- Background -->
+	<div class="fixed -z-10 h-screen w-screen bg-[url(/svg/graph.svg)]"></div>
+	<p class="context">{data.casePlay?.prompt}</p>
+</main>
+<!-- END: Search Bar -->
