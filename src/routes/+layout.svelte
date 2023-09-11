d90e0ff9892d446d3338a5d8fcbf30b52e5b2f5d
harvanchik <66141166+harvanchik@users.noreply.github.com>
2023-09-11 12:52:02 -0700

    move app capable meta tags to layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,10 @@
 	import '../app.css';
 </script>
 
+<svelte:head>
+	<meta name="apple-mobile-web-app-capable" content="yes" />
+	<meta name="mobile-web-app-capable" content="yes" />
+	<link rel="apple-touch-icon" href="/static/app.png" />
+</svelte:head>
+
 <slot />
