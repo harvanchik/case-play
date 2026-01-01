51e2fe328c5dc93d328be7b389d2b80541786244
harvanchik <jh6@harvhouse.com>
2026-01-01 13:14:18 -0800

    added app icons

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,10 +2,4 @@
 	import '../app.css';
 </script>
 
-<svelte:head>
-	<meta name="apple-mobile-web-app-capable" content="yes" />
-	<meta name="mobile-web-app-capable" content="yes" />
-	<link rel="apple-touch-icon" href="/static/app.png" />
-</svelte:head>
-
 <slot />
