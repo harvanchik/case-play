06faee415ccfcde27fa4e8403b6dae410072de37
harvanchik <jh6@harvhouse.com>
2026-07-16 19:04:59 -0700

    Center builder independently of attribution

--- src/routes/play-builder/[playId]/+page.svelte
+++ src/routes/play-builder/[playId]/+page.svelte
@@ -15,14 +15,16 @@
 <main class="relative isolate min-h-screen w-full overflow-x-hidden bg-stone-100 p-4">
 	<div aria-hidden="true" class="pointer-events-none fixed inset-0 z-0 bg-[url(/svg/graph.svg)]"></div>
 	<div aria-hidden="true" class="pointer-events-none fixed inset-0 z-0 bg-stone-100/[97%]"></div>
-	<div class="relative z-10 grid w-full grid-cols-1 items-stretch gap-4 lg:absolute lg:inset-4 lg:grid-cols-[minmax(0,1fr)_clamp(180px,18vw,300px)]" style="min-height: calc(100vh - 2rem);">
+	<div class="relative z-10 grid w-full grid-cols-1 items-stretch gap-4 lg:absolute lg:inset-4 lg:w-auto lg:grid-cols-[minmax(0,1fr)_clamp(180px,18vw,300px)]" style="min-height: calc(100vh - 2rem);">
 		<div class="flex min-w-0 items-center justify-start">
-			<div class="min-w-0" style="width: min(100%, calc((100vh - 4rem) * 2.1));">
+			<div class="min-w-0 lg:relative" style="width: min(100%, calc((100vh - 4rem) * 2.1));">
 				<FlagFootballPlayBuilder initialDocument={data.initialDocument} savedPlayId={data.playId} />
-				<PlayBuilderAttribution />
+				<div class="lg:absolute lg:inset-x-0 lg:top-full">
+					<PlayBuilderAttribution />
+				</div>
 			</div>
 		</div>
-		<div class="flex min-w-0 items-center justify-center">
+		<div class="flex min-w-0 items-center justify-center lg:pr-2">
 			<PlayBuilderAd />
 		</div>
 	</div>
