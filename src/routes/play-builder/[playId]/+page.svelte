c90a6ac0ae1fb38fa9bb376367adf7e64e349a1b
harvanchik <jh6@harvhouse.com>
2026-07-16 19:39:31 -0700

    Add play builder page heading

--- src/routes/play-builder/[playId]/+page.svelte
+++ src/routes/play-builder/[playId]/+page.svelte
@@ -18,6 +18,9 @@
 	<div class="relative z-10 grid w-full grid-cols-1 items-stretch gap-4 lg:absolute lg:inset-4 lg:w-auto lg:grid-cols-[minmax(0,1fr)_clamp(180px,18vw,300px)]" style="min-height: calc(100vh - 2rem);">
 		<div class="flex min-w-0 items-center justify-start">
 			<div class="min-w-0 lg:relative" style="width: min(100%, calc((100vh - 4rem) * 2.1));">
+				<h1 class="font-dokdo pb-1 text-center text-3xl font-semibold text-stone-800 uppercase select-none text-shadow-md lg:absolute lg:inset-x-0 lg:bottom-full sm:text-4xl">
+					Flag Football Play Builder
+				</h1>
 				<FlagFootballPlayBuilder initialDocument={data.initialDocument} savedPlayId={data.playId} />
 				<div class="lg:absolute lg:inset-x-0 lg:top-full">
 					<PlayBuilderAttribution />
